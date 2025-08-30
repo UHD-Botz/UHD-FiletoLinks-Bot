2f04573b5d5e8f88bcf15fc605112631c4f0bf71
UHD Official <uhdnetwork@gmail.com>
2025-08-30 22:09:32 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import logging 
 import datetime, time, asyncio
 from pyrogram import Client, filters
@@ -49,9 +45,6 @@ async def pm_broadcast(bot, message):
     except Exception as e:
         print(f"error: {e}")
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 async def broadcast_messages(user_id, message):
     try:
@@ -73,4 +66,5 @@ async def broadcast_messages(user_id, message):
         return False, "Error"
     except Exception as e:
         return False, "Error"
+
 
