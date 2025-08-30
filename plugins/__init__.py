c23496a69a449c37cfcd1f1d81987b25662db10a
UHD Official <uhdnetwork@gmail.com>
2025-08-30 22:05:53 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from aiohttp import web
 from .route import routes
 
@@ -9,3 +5,4 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
