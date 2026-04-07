0939952da098f53c0da897d264598dd2b2d51ff5
UHD NETWORK <uhdnetwork@gmail.com>
2026-04-07 15:48:46 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,5 +1,4 @@
-# 🗿  Visit & Support us - @UHD_Official
-# ⚡️ Do Not Remove Credit - Made by @UHD_Bots
+# ⚡️ Do Not Remove Credit - Made by @UHDBots
 # 💬 For Any Help Join Support Group: @UHDBots_Support
 # 🚫 Removing or Modifying these Lines will Cause the bot to Stop Working.
 
